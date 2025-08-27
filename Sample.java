7c01b7aba48b88c34c801aa190a4fd6cc5f04486
NandiniAnnareddy <annareddynandni51@gmail.com>
2025-08-27 17:41:30 +0530

    added

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args) {
         // Printing your name
         System.out.println("Annareddy Nandini");
+        System.out.println("Annareddy Nandini2");
     }
 }
